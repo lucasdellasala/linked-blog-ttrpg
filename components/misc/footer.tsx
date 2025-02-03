c776a85edd4bd825267adf0346486da6dbc6b890
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2025-02-03 12:14:36 -0300

    change header and footer

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -1,25 +1,25 @@
-import PreviewLink from "./preview-link"
-
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <PreviewLink href="/statically-generated">Statically Generated</PreviewLink> with Next.js.
+        <div className="py-12 flex flex-col lg:flex-row items-center">
+          <h3 className="text-4xl lg:text-2xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            Created and mastered by Lucas Della Sala.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="#"
+              href="https://www.instagram.com/vlady.ts/"
+              target="_blank"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Instagram
             </a>
             <a
-              href={`https://github.com/matthewwong525/linked-blog-starter`}
-              className="mx-3 font-bold hover:underline"
+              href="https://github.com/lucasdellasala"
+              target="_blank"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              View on GitHub
+              GitHub
             </a>
           </div>
         </div>
